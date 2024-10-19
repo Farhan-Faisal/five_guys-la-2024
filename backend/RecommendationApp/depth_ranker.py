f83dd663aa7ba8345907762ccde880905007ee55
Li, Jiayi <76764434+jiayili7@users.noreply.github.com>
2024-10-19 14:26:11 -0700

    feat: ranker doc string

--- backend/RecommendationApp/depth_ranker.py
+++ backend/RecommendationApp/depth_ranker.py
@@ -2,7 +2,19 @@ import textstat
 
 class DepthRanker:
     def __init__(self):
+        """
+        Initializes the DepthRanker class.
+        """
         pass
 
-    def rank_dale_chall_readability(self, input):
+    def rank_dale_chall_readability(self, input: str) -> float:
+        """
+        Ranks the readability of the given text using the Dale-Chall readability formula.
+
+        Args:
+            input (str): The text to be ranked.
+
+        Returns:
+            float: The Dale-Chall readability score of the input text.
+        """
         return textstat.dale_chall_readability_score(input)
