171d7f10ab3b8e7361a813f3e26d48659d7c25a7
farhan64 <farhan64@student.ubc.ca>
2024-10-22 11:54:57 -0700

    v19

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -38,16 +38,38 @@ const Home = () => {
   }, []); // Empty dependency array to run once on mount
 
   if (loading) {
-    return <div className="flex justify-center items-center h-screen">Loading...</div>; // Show loading indicator
+    return (
+      <div className="flex justify-center items-center h-screen">
+        Loading...
+      </div> // Show loading indicator
+    );
   }
 
   return (
-    <div className="flex h-screen bg-gray-100">
-      {/* Left side (Discussion Titles) */}
-      <div className="w-1/2 flex flex-col border-r border-gray-300 bg-white">
+    <div className="h-screen flex justify-items-center bg-gray-100">
+      {/* LEFT side (COURSES) */}
+      <div className="w-1/2 flex flex-col p-4 m-4 rounded border-black justify-center bg-white">
+        <div className="p-4 bg-blue-500 text-white text-center font-bold">
+          Courses
+        </div>
+
+        <div className="flex-1 p- 4 overflow-y-auto">
+          <div className="space-y-4">
+            <Link key="a" href={`/discussions`}>
+              <div className="p-3 bg-gray-200 rounded-lg m-2 cursor-pointer hover:bg-gray-300">
+                <strong>Learning Analytics</strong>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+      
+      {/* RIGHT side (DISCUSSION TITLES) */}
+      <div className="w-1/2 flex flex-col p-4 m-4 rounded border-black justify-center bg-white">
         <div className="p-4 bg-blue-500 text-white text-center font-bold">
           Discussion Titles
         </div>
+        
         <div className="flex-1 p- 4 overflow-y-auto">
           {/* Display discussion titles in a vertical list */}
           <div className="space-y-4">
@@ -61,12 +83,11 @@ const Home = () => {
             ))}
           </div>
         </div>
+        
       </div>
 
-      {/* Right side (Empty for now, reserved for future content) */}
-      <div className="w-1/2 p-4 bg-gray-50">
-        {/* Future content can be placed here */}
-      </div>
+     
+
     </div>
   );
 };
